dac77a373c63c4c427bfc8afd5751b482386f0e7
IessaOlarm <eng.iessa.mostafa@gmail.com>
2019-02-10 06:50:41 +0200

    functionality

--- SpiceKitchen.sql
+++ SpiceKitchen.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Jan 25, 2019 at 07:41 PM
+-- Generation Time: Feb 10, 2019 at 05:47 AM
 -- Server version: 10.1.28-MariaDB
 -- PHP Version: 7.1.10
 
@@ -21,6 +21,51 @@ SET time_zone = "+00:00";
 --
 -- Database: `SpiceKitchen`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bg_images`
+--
+
+CREATE TABLE `bg_images` (
+  `bgImages_id` int(10) UNSIGNED NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `bgImages` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `bg_images`
+--
+
+INSERT INTO `bg_images` (`bgImages_id`, `created_at`, `updated_at`, `bgImages`) VALUES
+(2, NULL, NULL, '01.jpg'),
+(3, NULL, NULL, '02.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contact_uses`
+--
+
+CREATE TABLE `contact_uses` (
+  `contactUsid` int(3) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `message` text CHARACTER SET utf8mb4,
+  `phone` int(191) DEFAULT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `email` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `contact_uses`
+--
+
+INSERT INTO `contact_uses` (`contactUsid`, `created_at`, `updated_at`, `message`, `phone`, `name`, `email`) VALUES
+(3, '2019-02-05 04:46:27', '2019-02-05 04:46:27', 'qw3fev', 1002433739, 'Iessa darktroy', 'eng.iessa.mostafa@gmail.com'),
+(4, '2019-02-05 04:46:54', '2019-02-05 04:46:54', 'AWEVFQ3GQERWBGWEQARGAQERGAWREG', 1002433739, 'Iessa darktroy', 'eng.iessa.mostafa@gmail.com');
 
 -- --------------------------------------------------------
 
@@ -52,7 +97,54 @@ INSERT INTO `galleries` (`galleryId`, `created_at`, `updated_at`, `image`) VALUE
 (12, NULL, NULL, 'g08.jpg'),
 (13, NULL, NULL, 'g09.jpg'),
 (14, NULL, NULL, 'g10.jpg'),
-(15, NULL, NULL, 'g11.jpg');
+(15, NULL, NULL, 'g11.jpg'),
+(16, '2019-01-27 01:53:04', '2019-01-27 01:53:04', 'menuItems1548561184.png'),
+(17, '2019-01-27 02:09:15', '2019-01-27 02:09:15', 'menuItems1548562155.png');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `homepages`
+--
+
+CREATE TABLE `homepages` (
+  `homepageid` int(3) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `p1` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `p2` text COLLATE utf8mb4_unicode_ci,
+  `title` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `homepages`
+--
+
+INSERT INTO `homepages` (`homepageid`, `created_at`, `updated_at`, `p1`, `p2`, `title`) VALUES
+(1, NULL, '2019-02-07 01:50:48', 'Welcome to The Spice Kitchen! As\r\nhistory puts it, the Hakkas who left\r\nSouthern China and settled in India\r\nloved eating the local spices and\r\nincorporated it with their cooking\r\nstyle since the early 19th century.', 'An authentic Indian-Chinese,\r\nIndian and Thai restaurant where\r\ntraditional Hakka recipes infused\r\nwith Indian spices take dishes to\r\nanother level of culinary delights\r\nand set to tickle your palate with', 'Hi There');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `menus`
+--
+
+CREATE TABLE `menus` (
+  `menu_id` int(11) NOT NULL,
+  `menu_name` varchar(64) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `menus`
+--
+
+INSERT INTO `menus` (`menu_id`, `menu_name`, `created_at`, `updated_at`) VALUES
+(1, 'Starters', '2019-02-08 05:37:31', '0000-00-00 00:00:00'),
+(2, 'Soup', '2019-02-08 05:37:39', '0000-00-00 00:00:00'),
+(3, 'Indian Main Cuisine', '2019-02-09 03:28:26', '0000-00-00 00:00:00'),
+(4, 'Indian Chinese Cuisine', '2019-02-09 03:28:20', '0000-00-00 00:00:00');
 
 -- --------------------------------------------------------
 
@@ -68,23 +160,35 @@ CREATE TABLE `menu_items` (
   `title` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `menutypeId` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `itemDescription` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `price` int(3) NOT NULL
+  `price` int(3) NOT NULL,
+  `memberprice` int(2) NOT NULL,
+  `menu_id` int(11) NOT NULL,
+  `submenuid` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Dumping data for table `menu_items`
 --
 
-INSERT INTO `menu_items` (`menuItemsId`, `created_at`, `updated_at`, `image`, `title`, `menutypeId`, `itemDescription`, `price`) VALUES
-(1, NULL, '2019-01-24 05:22:44', NULL, 'title test 2 up', '1', 'description ,efokmjv test', 5),
-(2, '2019-01-24 07:38:45', '2019-01-24 07:38:45', 'menuItems1548322725.png', 'title 2', '1', 'item description 2', 23),
-(3, '2019-01-24 07:39:52', '2019-01-24 07:39:52', 'menuItems1548322792.png', 'title ..3', '1', 'item description 3', 14),
-(4, '2019-01-24 07:40:06', '2019-01-24 07:40:06', 'menuItems1548322806.png', 'title 4', '2', 'item description 4', 52),
-(5, '2019-01-24 07:40:27', '2019-01-24 07:40:27', 'menuItems1548322827.png', 'title 5', '2', 'item description 5', 8),
-(11, '2019-01-24 07:41:30', '2019-01-24 07:41:30', 'menuItems1548322890.png', 'title ..test 11', '2', 'item description testitem description testitem description testitem description test 11', 30),
-(12, '2019-01-24 07:38:45', '2019-01-24 07:38:45', 'menuItems1548322725.png', 'title 2 menu type 3', '3', 'item description 2', 23),
-(14, '2019-01-24 07:40:06', '2019-01-24 07:40:06', 'menuItems1548322806.png', 'title 4 menu type 3', '3', 'item description 4', 52),
-(20, '2019-01-24 07:41:30', '2019-01-24 07:41:30', 'menuItems1548322890.png', 'title ..test 10 menu type 3', '3', 'item description testitem description testitem description testitem  menu type 3 description test 10', 46);
+INSERT INTO `menu_items` (`menuItemsId`, `created_at`, `updated_at`, `image`, `title`, `menutypeId`, `itemDescription`, `price`, `memberprice`, `menu_id`, `submenuid`) VALUES
+(1, NULL, '2019-01-24 05:22:44', NULL, 'title test 2 up', '1', 'description ,efokmjv test', 5, 0, 1, NULL),
+(2, '2019-01-24 07:38:45', '2019-01-24 07:38:45', 'menuItems1548322725.png', 'title 2', '1', 'item description 2', 23, 0, 1, NULL),
+(3, '2019-01-24 07:39:52', '2019-01-24 07:39:52', 'menuItems1548322792.png', 'title ..3', '1', 'item description 3', 14, 0, 2, NULL),
+(4, '2019-01-24 07:40:06', '2019-01-24 07:40:06', 'menuItems1548322806.png', 'title 4', '2', 'item description 4', 52, 0, 1, NULL),
+(5, '2019-01-24 07:40:27', '2019-01-24 07:40:27', 'menuItems1548322827.png', 'title 5', '2', 'item description 5', 8, 0, 2, NULL),
+(11, '2019-01-24 07:41:30', '2019-01-24 07:41:30', 'menuItems1548322890.png', 'title ..test 11', '2', 'item description testitem description testitem description testitem description test 11', 30, 0, 2, NULL),
+(12, '2019-01-24 07:38:45', '2019-01-24 07:38:45', 'menuItems1548322725.png', 'title 2 menu type 3', '3', 'item description 2', 23, 0, 2, NULL),
+(14, '2019-01-24 07:40:06', '2019-01-24 07:40:06', 'menuItems1548322806.png', 'title 4 menu type 3', '3', 'item description 4', 52, 0, 2, NULL),
+(20, '2019-01-24 07:41:30', '2019-01-24 07:41:30', 'menuItems1548322890.png', 'title ..test 10 menu type 3', '3', 'item description testitem description testitem description testitem  menu type 3 description test 10', 46, 0, 2, NULL),
+(22, '2019-01-30 00:53:37', '2019-01-30 01:05:49', NULL, '30', '1', '130', 15, 0, 1, NULL),
+(23, NULL, '2019-01-24 05:22:44', NULL, 'title test 2 up3', '1', 'description ,efokmjv test', 5, 0, 1, NULL),
+(24, NULL, '2019-01-24 05:22:44', NULL, 'title test 2 u4p3', '1', 'description ,efokmjv test', 5, 0, 1, NULL),
+(25, NULL, '2019-01-24 05:22:44', NULL, 'title test 5', '1', 'description ,efokmjv test', 5, 0, 1, NULL),
+(26, NULL, '2019-01-24 05:22:44', NULL, 'title test665', '1', 'description ,efokmjv test', 5, 0, 1, NULL),
+(27, NULL, '2019-01-24 05:22:44', NULL, 'title test62265', '1', 'description ,efokmjv test', 5, 0, 1, NULL),
+(28, NULL, '2019-01-24 05:22:44', NULL, 'title 55', '1', 'description ,efokmjv test', 5, 0, 1, NULL),
+(29, NULL, '2019-01-24 05:22:44', NULL, 'title test 2 up', '1', 'description ,efokmjv test', 5, 0, 1, NULL),
+(30, '2019-02-09 05:35:58', '2019-02-09 05:35:58', NULL, 'qewdtitle ...', '1', 'Description ...qwacd', 3, 9, 1, 0);
 
 -- --------------------------------------------------------
 
@@ -120,16 +224,6 @@ CREATE TABLE `migrations` (
   `batch` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
---
--- Dumping data for table `migrations`
---
-
-INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
-(1, '2014_10_12_100000_create_password_resets_table', 1),
-(2, '2019_01_23_045945_create_menu_types_table', 2),
-(3, '2019_01_23_050557_create_menu_items_table', 3),
-(4, '2019_01_23_050823_create_galleries_table', 3);
-
 -- --------------------------------------------------------
 
 --
@@ -141,6 +235,31 @@ CREATE TABLE `password_resets` (
   `token` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `submenus`
+--
+
+CREATE TABLE `submenus` (
+  `submenuid` int(3) NOT NULL,
+  `menu_id` int(2) NOT NULL,
+  `submenu_name` varchar(64) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `submenus`
+--
+
+INSERT INTO `submenus` (`submenuid`, `menu_id`, `submenu_name`, `created_at`, `updated_at`) VALUES
+(1, 3, 'Lamb Dishes', '2019-02-09 03:40:48', '0000-00-00 00:00:00'),
+(2, 3, 'SeaFood Dishes', '2019-02-09 03:40:48', '0000-00-00 00:00:00'),
+(3, 3, 'Vegetarian Dishes', '2019-02-09 03:41:23', '0000-00-00 00:00:00'),
+(4, 4, 'Vegetable Dishes', '2019-02-09 03:41:23', '0000-00-00 00:00:00'),
+(5, 4, 'Chicken Dishes', '2019-02-09 03:41:45', '0000-00-00 00:00:00');
 
 -- --------------------------------------------------------
 
@@ -171,10 +290,34 @@ INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `re
 --
 
 --
+-- Indexes for table `bg_images`
+--
+ALTER TABLE `bg_images`
+  ADD PRIMARY KEY (`bgImages_id`);
+
+--
+-- Indexes for table `contact_uses`
+--
+ALTER TABLE `contact_uses`
+  ADD PRIMARY KEY (`contactUsid`);
+
+--
 -- Indexes for table `galleries`
 --
 ALTER TABLE `galleries`
   ADD PRIMARY KEY (`galleryId`);
+
+--
+-- Indexes for table `homepages`
+--
+ALTER TABLE `homepages`
+  ADD PRIMARY KEY (`homepageid`);
+
+--
+-- Indexes for table `menus`
+--
+ALTER TABLE `menus`
+  ADD PRIMARY KEY (`menu_id`);
 
 --
 -- Indexes for table `menu_items`
@@ -201,6 +344,12 @@ ALTER TABLE `password_resets`
   ADD KEY `password_resets_email_index` (`email`);
 
 --
+-- Indexes for table `submenus`
+--
+ALTER TABLE `submenus`
+  ADD PRIMARY KEY (`submenuid`);
+
+--
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
@@ -211,16 +360,40 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `bg_images`
+--
+ALTER TABLE `bg_images`
+  MODIFY `bgImages_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `contact_uses`
+--
+ALTER TABLE `contact_uses`
+  MODIFY `contactUsid` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
 -- AUTO_INCREMENT for table `galleries`
 --
 ALTER TABLE `galleries`
-  MODIFY `galleryId` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `galleryId` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+
+--
+-- AUTO_INCREMENT for table `homepages`
+--
+ALTER TABLE `homepages`
+  MODIFY `homepageid` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `menus`
+--
+ALTER TABLE `menus`
+  MODIFY `menu_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `menu_items`
 --
 ALTER TABLE `menu_items`
-  MODIFY `menuItemsId` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+  MODIFY `menuItemsId` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
 
 --
 -- AUTO_INCREMENT for table `menu_types`
@@ -232,7 +405,13 @@ ALTER TABLE `menu_types`
 -- AUTO_INCREMENT for table `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `submenus`
+--
+ALTER TABLE `submenus`
+  MODIFY `submenuid` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `users`
